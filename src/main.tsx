6e21f949f5ccccde45e579aab35d59d91b531d6a
Mykyta Batrak <mykbatrak@gmail.com>
2024-11-13 15:42:00 -0600

    Updated examples to showcase race condition when sorting imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,52 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import { StrictMode, useEffect, useLayoutEffect } from "react";
-import vite from "@vitejs/plugin-react-swc";
-import "./index.css";
-import { renderToString } from "react-dom/server";
-import { searchForWorkspaceRoot } from "vite";
+import { StrictMode } from "react";
 
-useMemo();
-useLayoutEffect();
-renderToString(element);
-searchForWorkspaceRoot();
+import Rating from "@mui/material/Rating";
+import RadioGroup from "@mui/material/RadioGroup";
+import Radio from "@mui/material/Radio";
+import LinearProgress from "@mui/material/LinearProgress";
+import Fab from "@mui/material/Fab";
+import Dialog from "@mui/material/Dialog";
+import Checkbox from "@mui/material/Checkbox";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
+import Autocomplete from "@mui/material/Autocomplete";
+import { Alert, Avatar, Backdrop, Badge, Chip, Divider, Icon, List, SvgIcon, Table, TextField, ToggleButton, Tooltip, Typography } from "@mui/material";
+import Select from "@mui/material/Select";
+import Slider from "@mui/material/Slider";
+import Switch from "@mui/material/Switch";
+import App from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Autocomplete />
+    <ButtonGroup />
+    <Button />
+    <Checkbox />
+    <Fab />
+    <Radio />
+    <RadioGroup />
+    <Rating />
+    <Select />
+    <Slider />
+    <Switch />
+    <TextField />
+    <ToggleButton />
+    <Avatar />
+    <Badge />
+    <Chip />
+    <Divider />
+    <Icon />
+    <SvgIcon />
+    <List />
+    <Table />
+    <Tooltip />
+    <Typography />
+    <Alert />
+    <Backdrop />
+    <Dialog />
+    <LinearProgress />
   </StrictMode>,
 );
